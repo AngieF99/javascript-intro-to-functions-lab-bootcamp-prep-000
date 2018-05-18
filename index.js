e080269d311dde9efedd90aa158d8d451d8f889b
Angelica Fields <angelicafields08@gmail.com>
2018-05-18 15:12:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ if(string === string.toLowerCase()){
 } else if(string == "I love you, Grandma."){
   return  "I love you, too.";
   } else if (string === string.toUpperCase()){
-    return "yes indeed!"
+    return "YES INDEED!"
   }
   }
