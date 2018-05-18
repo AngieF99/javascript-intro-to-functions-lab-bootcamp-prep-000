5746220d0996844b35e548ab3cedaaba89485af4
Angelica Fields <angelicafields08@gmail.com>
2018-05-18 15:08:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-if(string = string.toLowerCase()){
+if(string === string.toLowerCase()){
   return "I can't hear you!";
-}
-}
-
-function sayHiToGrandma(string) {
-  if(string = "I love you, Grandma."){
+} else if(string = "I love you, Grandma."){
   return  "I love you, too.";
+  } else if(string === string.toUpperCase){
+    return "YES INDEED!"
   }
-
-}
+  }
